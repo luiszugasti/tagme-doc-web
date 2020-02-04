7c22641734f28901922a1013013525925a200c06
Luis Zugasti <luis.zugasti@outlook.com>
2020-02-03 21:25:41 -0500

    flush out graph definition further

--- document-comparison.py
+++ document-comparison.py
@@ -20,12 +20,33 @@ Builds a comparison structure between documents that should be easily relatable.
 class Graph:
     def __init__(self):
         # Hashed dictionary of nodes - it will get big!
-        self.nodes = {}
+        self.nodes = {} # nodeID : node -> doc_name : structure with doc_name, and edges
+
+    def add_node(self, node):
+        # add a single key to the dictionary
+        if nodeID in self.nodes:
+            print("Apparently you're adding a duplicate node? Not allowed\n")
+        else:
+            self.nodes[nodeID] = node # just a one, makes the data structure somewhat simpler?
+
+# I feel I should be throwing these as exceptions... maybe if this was production ready code
 
 
 class Node:
-    def __init__(self):
+    def __init__(self, nodeID):
         self.edges = {}
+        self.nodeID = nodeID
+
+    def add_edge(self, edge):
+        """
+
+        :type edge: Edge
+        """
+        # key for the edge will be the destination
+        if edge.ending_node in self.edges:
+            print("You're trying to add a duplicate edge. Not allowed\n")
+        else:
+            self.edges[edge.ending_node] = edge
 
 
 class Edge:
@@ -42,19 +63,10 @@ def main(disallow_insignificant_weights):
     # Set the authorization token for subsequent calls globally
     tagme.GCUBE_TOKEN = "1c7074e0-10bb-4131-a498-5179035a001a-843339462"
 
-    with io.open("shortrun.json", encoding='utf-8') as file:
+    with io.open("production.json", encoding='utf-8') as file:
         document_string = file.read().encode('utf-8')
 
     document_corpus = json.loads(document_string, encoding="utf-8")
-    # Loads our dictionary. Struct is document : annotation : count
-    # You can submit a list of pairs of any size, but the TagMe web
-    # service will be issued one query every 100 pairs. If one entity does not exist, the result will be None.
-
-    # First, I'll find all the entities that are common between each doc. Sets.
-    # Then, build unique tuples, create a list of these tuples, send to TAGME.
-    # TAGME returns the relatedness between each of them.
-    # Add them all, and divide them by the size of the TAGME packet.
-    # Append this to my Graph.
 
     doc_map = common_between_dicts(document_corpus, disallow_insignificant_weights)
 
@@ -80,18 +92,6 @@ def common_between_dicts(dictionary, disallow_insignificant_weights):
 
     print("Job to get edges completed in {0} seconds\n# of Operations: {1}".format(str(elapsed_time),
                                                                                    str(len(edge_list))))
-    common_tuple = ()
-
-    # for iterable in dictionary.iteritems():
-    #     for iterable2 in dictionary.iteritems():
-    #         # compare two different documents
-    #         if iterable != iterable2:
-    #             # Now iterate through the iterable and iterable2 to build tuples of all their entities:
-    #             # format: (iterable_entity : iterable2_entity)
-    #             # for each of this iterable, we'll loop through ALL the iterable2 possibles (thus threaded approach!)
-    #             # for each built tuple that we get, we send it to tagme, and in return, add this edge to the node.
-    #             # Since we want to not go over a document again (and do n! instead of n^2 complexity)
-    # We have to mark a document
 
     return 1
 
@@ -135,7 +135,7 @@ def get_entity_relatedness(list_of_tuples, time_to_wait=1):
     return entity_ratings
 
 
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+def id_generator(size=7, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
